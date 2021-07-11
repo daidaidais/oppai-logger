3142f841749b629fca5ab2d63e98e63887289805
daidaidais <daisuke4yukita@gmail.com>
2021-07-11 09:46:07 +0900

    add MutateDatabase component to make sure the current date is loaded on click

--- src/components/AddNew.js
+++ src/components/AddNew.js
@@ -1,56 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Styles from "./AddNew.module.css";
-
-import firebase from "firebase/app";
-import "firebase/database";
-import {
-  FirebaseDatabaseProvider,
-  FirebaseDatabaseMutation,
-} from "@react-firebase/database";
+import MutateDatabase from "./MutateDatabase.js";
 
 const AddNew = ({ firebaseConfig }) => {
-  const getMYD = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1 < 10 ? "0" : "") + (date.getMonth() + 1);
-    const day = (date.getDate() < 10 ? "0" : "") + date.getDate();
-    const formattedMYD = `${year}${month}${day}`;
-    return formattedMYD;
-  };
+  const [clicked, setClicked] = useState(false);
 
   return (
-    <FirebaseDatabaseProvider firebase={firebase} {...firebaseConfig}>
-      <Row className={Styles.wrapper}>
-        <Col
-          xs={{ span: 10, offset: 1 }}
-          sm={{ span: 8, offset: 2 }}
-          md={{ span: 8, offset: 2 }}
-          lg={{ span: 8, offset: 2 }}
+    <Row className={Styles.wrapper}>
+      <Col
+        xs={{ span: 10, offset: 1 }}
+        sm={{ span: 8, offset: 2 }}
+        md={{ span: 8, offset: 2 }}
+        lg={{ span: 8, offset: 2 }}
+      >
+        <Button
+          variant="dark"
+          size="lg"
+          block
+          className={Styles.button}
+          onClick={() => {
+            setClicked(true);
+          }}
         >
-          <FirebaseDatabaseMutation type="push" path={`oppai/${getMYD()}`}>
-            {({ runMutation }) => (
-              <Button
-                variant="dark"
-                size="lg"
-                block
-                className={Styles.button}
-                onClick={async () => {
-                  await runMutation({
-                    //MYD: getMYD(),
-                    createdAt: firebase.database.ServerValue.TIMESTAMP,
-                  });
-                }}
-              >
-                授乳します
-              </Button>
-            )}
-          </FirebaseDatabaseMutation>
-        </Col>
-      </Row>
-    </FirebaseDatabaseProvider>
+          授乳します
+        </Button>
+        {clicked && (
+          <MutateDatabase
+            firebaseConfig={firebaseConfig}
+            setClicked={setClicked}
+          />
+        )}
+      </Col>
+    </Row>
   );
 };
 
---
+++ src/components/MutateDatabase.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+
+import firebase from "firebase/app";
+import "firebase/database";
+import {
+  FirebaseDatabaseProvider,
+  FirebaseDatabaseMutation,
+} from "@react-firebase/database";
+
+const MutateDatabase = ({ firebaseConfig, setClicked }) => {
+  useEffect(() => {
+    //console.log(`MutateDatabse mounted`);
+
+    return () => {
+      //console.log(`MutateDatabse unmounted`);
+    };
+  }, []);
+
+  const getMYD = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1 < 10 ? "0" : "") + (date.getMonth() + 1);
+    const day = (date.getDate() < 10 ? "0" : "") + date.getDate();
+    const formattedMYD = `${year}${month}${day}`;
+    return formattedMYD;
+  };
+
+  return (
+    <FirebaseDatabaseProvider firebase={firebase} {...firebaseConfig}>
+      <FirebaseDatabaseMutation type="push" path={`oppai/${getMYD()}`}>
+        {({ runMutation }) => {
+          const mutation = async () => {
+            await runMutation({
+              //MYD: getMYD(),
+              createdAt: firebase.database.ServerValue.TIMESTAMP,
+            });
+            await setClicked(false);
+          };
+          setTimeout(() => {
+            mutation();
+          }, 500);
+          return <></>;
+        }}
+      </FirebaseDatabaseMutation>
+    </FirebaseDatabaseProvider>
+  );
+};
+
+export default MutateDatabase;
